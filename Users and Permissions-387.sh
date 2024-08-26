63b7b068645ae5eaa896920c2ba6f7b3158b5ea0
a-xitas <a.xitas@gmail.com>
2024-08-26 12:41:27 +0100

    Users and Permissions

---
+++ Users and Permissions-387.sh
@@ -0,0 +1,35 @@
+## 1. Users ##
+
+/home/dq$ id
+
+## 2. Groups ##
+
+/home/dq$ groups
+
+## 3. Revisiting ls ##
+
+/$ cd /root
+
+## 4. Permissions ##
+
+/$ cd ~
+
+## 5. Changing Permissions: Symbolic Notation ##
+
+/home/dq$ chmod u+x,g+x,o+w disposable*
+
+## 6. The File Status Command ##
+
+/home/dq$ answer 0776
+
+## 7. Changing Permissions: Octal Notation ##
+
+/home/dq$ answer 602 171 ----wxr-x
+
+## 8. The Superuser ##
+
+/home/dq$ sudo cal
+
+## 9. Changing Ownership ##
+
+/home/dq$ history
