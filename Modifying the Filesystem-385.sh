913dee774dc67a9c7615be20f10515f3eb57839c
a-xitas <a.xitas@gmail.com>
2024-08-26 12:40:33 +0100

    Modifying the Filesystem

---
+++ Modifying the Filesystem-385.sh
@@ -0,0 +1,31 @@
+## 1. Creating Directories ##
+
+/home/dq$ mkdir dir2 dir3 dir5
+
+## 2. Deleting Directories ##
+
+/home/dq$ rmdir dir2 dir3 dir5
+
+## 3. Failing to Delete Directories ##
+
+/home/dq$ rmdir prize_winners/
+
+## 4. Copying Files ##
+
+/home/dq$ cp augustus violet veruca tv brats
+
+## 5. Hidden Dangers of Copying Files ##
+
+/home/dq/prize_winners$ ls -A
+
+## 6. Copying Directories ##
+
+/home/dq$ cp -R prize_winners brats
+
+## 7. Deleting Directories and Files ##
+
+/home/dq$ ls
+
+## 8. Moving and Renaming Directories and Files ##
+
+/home/dq/brats/prize_winners$ mv .mike Mike
