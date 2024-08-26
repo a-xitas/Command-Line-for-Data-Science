e3329926ba2865ad1db8c5bc0624b4e06eba095f
a-xitas <a.xitas@gmail.com>
2024-08-26 12:40:11 +0100

    The Filesystem

---
+++ The Filesystem-384.sh
@@ -0,0 +1,27 @@
+## 1. The Filesystem ##
+
+/home/dq$ answer false true true
+
+## 2. Working Directory ##
+
+/home/dq$ pwd
+
+## 3. Inspecting Directories ##
+
+/home/dq$ ls
+
+## 4. Inspecting Directories Thoroughly ##
+
+/home/dq$ ls prize_winners
+
+## 5. Navigating the Filesystem ##
+
+/home/dq$ cd prize_winners
+
+## 6. Absolute and Relative Paths ##
+
+/home$ answer false true false
+
+## 7. Shortcuts for Filesystem Navigation ##
+
+/home$ ls -A /home/dq/prize_winners
